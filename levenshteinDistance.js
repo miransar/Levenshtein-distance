335b251bf638a3783844ed452decc5cedf4c19fe
Mir Ansar <ansar.ali18@gmail.com>
2017-12-25 03:00:01 +0530

    fixed indents with lint

--- levenshteinDistance.js
+++ levenshteinDistance.js
@@ -1,39 +1,38 @@
+'use strict';
 
+function levenshtein (ccName, subscriberName) {
+	if (!ccName || !subscriberName) return -1;
 
-function levenshtein(ccName, subscriberName){
-    if(!ccName || !subscriberName ) return -1; 
+	const difference = [];
 
-    var difference = [];
-    
-    var i;
-    for(i = 0; i <= subscriberName.length; i++){
-      difference[i] = [i];
-    }
-  
-    var j;
-    for(j = 0; j <= ccName.length; j++){
-      difference[0][j] = j;
-    }
-  
-    for(i = 1; i <= subscriberName.length; i++){
-      for(j = 1; j <=ccName.length; j++){
-        if(subscriberName.charAt(i-1) ==ccName.charAt(j-1)){
-          difference[i][j] = difference[i-1][j-1];
-        } else {
-          difference[i][j] = Math.min(difference[i-1][j-1] + 1, 
-                                  Math.min(difference[i][j-1] + 1, 
-                                    difference[i-1][j] + 1));
-        }
-      }
-    }
+	let i;
+	for (i = 0; i <= subscriberName.length; i++) {
+		difference[i] = [i];
+	}
 
-  var levDis = difference[subscriberName.length][ccName.length]
-  var bigger = Math.max(ccName.length, subscriberName.length)
-  var value = (bigger - levDis) / bigger
-  
-    return value.toLocaleString("en", {style: "percent"});
-  };
+	let j;
+	for (j = 0; j <= ccName.length; j++) {
+		difference[0][j] = j;
+	}
 
+	for (i = 1; i <= subscriberName.length; i++) {
+		for (j = 1; j <= ccName.length; j++) {
+			if (subscriberName.charAt(i - 1) == ccName.charAt(j - 1)) {
+				difference[i][j] = difference[i - 1][j - 1];
+			} else {
+				difference[i][j] = Math.min(difference[i - 1][j - 1] + 1,
+                                  Math.min(difference[i][j - 1] + 1,
+                                    difference[i - 1][j] + 1));
+			}
+		}
+	}
+
+	const distance = difference[subscriberName.length][ccName.length];
+	const max = Math.max(ccName.length, subscriberName.length);
+	const value = (max - distance) / max;
+
+	return value.toLocaleString('en', { style: 'percent' });
+}
 
 
 console.log(levenshtein(process.argv[2], process.argv[3]));
