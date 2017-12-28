cefc2d780663b1a9e5342fb4a494c5da14b97f67
Mir-Ansar <ansar.ali18@gmail.com>
2017-12-28 14:42:48 +0530

    changed argument names

--- levenshteinDistance.js
+++ levenshteinDistance.js
@@ -1,23 +1,23 @@
 'use strict';
 
-module.exports = function levenshtein (ccName, subscriberName) {
-	if (!ccName || !subscriberName) return 0;
+module.exports = function levenshtein (input1, input2) {
+	if (!input1 || !input2) return 0;
 
 	const difference = [];
 
 	let i;
-	for (i = 0; i <= subscriberName.length; i++) {
+	for (i = 0; i <= input2.length; i++) {
 		difference[i] = [i];
 	}
 
 	let j;
-	for (j = 0; j <= ccName.length; j++) {
+	for (j = 0; j <= input1.length; j++) {
 		difference[0][j] = j;
 	}
 
-	for (i = 1; i <= subscriberName.length; i++) {
-		for (j = 1; j <= ccName.length; j++) {
-			if (subscriberName.charAt(i - 1) === ccName.charAt(j - 1)) {
+	for (i = 1; i <= input2.length; i++) {
+		for (j = 1; j <= input1.length; j++) {
+			if (input2.charAt(i - 1) === input1.charAt(j - 1)) {
 				difference[i][j] = difference[i - 1][j - 1];
 			} else {
 				difference[i][j] = Math.min(difference[i - 1][j - 1] + 1,
@@ -27,9 +27,11 @@ module.exports = function levenshtein (ccName, subscriberName) {
 		}
 	}
 
-	const distance = difference[subscriberName.length][ccName.length];
-	const max = Math.max(ccName.length, subscriberName.length);
+	const distance = difference[input2.length][input1.length];
+	const max = Math.max(input1.length, input2.length);
 	const value = (max - distance) / max;
 
-	return value * 100;
+	// value is converted to percentage
+
+	return Math.round(value * 100);
 };
